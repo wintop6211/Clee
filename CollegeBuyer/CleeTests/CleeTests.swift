56f31fd33b3103233927e3255c8a13ac52083753
liangy <liangy@msoe.edu>
2018-11-24 18:47:34 -0600

    re-added test builds to the project so that no compile errors appear

--- CollegeBuyer/CleeTests/CleeTests.swift
+++ CollegeBuyer/CleeTests/CleeTests.swift
@@ -1,36 +1,33 @@
 //
-//  CollegeBuyerTests.swift
-//  CollegeBuyerTests
+//  CleeTests.swift
+//  CleeTests
 //
-//  Created by Yahui Liang on 6/10/17.
-//  Copyright © 2017 CollegeBuyer. All rights reserved.
+//  Created by Yahui Liang on 11/24/18.
+//  Copyright © 2018 CollegeBuyer. All rights reserved.
 //
 
 import XCTest
-@testable import Clee
 
 class CleeTests: XCTestCase {
-    
+
     override func setUp() {
-        super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
     }
-    
+
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-    
+
 }
